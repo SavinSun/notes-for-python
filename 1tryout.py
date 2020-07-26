4c9c860ec9a74055780229796961312fe7d057f5
SavinSun <sunnima666@gmail.com>
2020-07-26 19:27:30 +0800

    first commit

--- 1tryout.py
+++ 1tryout.py
@@ -16,3 +16,4 @@ print(top())
 #————————————————
 #版权声明：本文为CSDN博主「UndeFIned丶」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
 #原文链接：https://blog.csdn.net/weixin_46313446/article/details/105510984
+#edit 1st time
